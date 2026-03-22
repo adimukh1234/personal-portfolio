0b67efd43eff57e7f8bc56df34bfca135377701b
Aditya Mukherjee <adimukherjee100@gmail.com>
2026-03-22 23:34:15 +0530

    removed unnecessary imports and dead code

--- components/AudioSystem.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-
-// This is a placeholder component for audio system functionality
-// Most audio logic is handled in the main SciFiLanding component
-export function AudioSystem() {
-  return null;
-}
